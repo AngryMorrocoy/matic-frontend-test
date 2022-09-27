d8c798a2e755fb994b2e26890e8cf6ee57cd908d
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-09-27 01:36:25 -0400

    + ArticleList component

--- src/components/molecules/ArticleList/index.tsx
+++ src/components/molecules/ArticleList/index.tsx
@@ -2,22 +2,45 @@ import { ArticleSchema } from '@api/types';
 import { Table, TBody, THead, TRow } from '@atoms/Table';
 import { GridWrapper } from '@atoms/Wrappers';
 import { formatDateAsDDYYMM } from '@utils/index';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, MouseEvent } from 'react';
 import { ActionButton, StyledTRow } from './ArticleList.styles';
 
 type ArticleListProps = {
   className?: string;
   articles: ArticleSchema[];
+  onEdit: (articleId: string) => void;
+  onDelete: (articleId: string) => void;
 };
 
 const ArticleList: FunctionComponent<ArticleListProps> = ({
   className,
   articles,
+  onDelete,
+  onEdit,
 }): JSX.Element => {
   const columnTitles = ['author name', 'title', 'content', 'date'];
 
+  const handleClick = (evt: MouseEvent<HTMLTableElement>) => {
+    const target = evt.target as HTMLElement;
+    if (!target.classList.contains('action-button')) return;
+
+    const action = target.getAttribute('action');
+
+    // Go up in the elements tree until finds the first 'tr' parent
+    let curr = target;
+    while (curr && curr.nodeName.toLowerCase() !== 'tr') {
+      curr = curr.parentNode as HTMLElement;
+    }
+
+    // Grab the id of that 'tr' tag (Which is the one of an article)
+    let articleId = curr.id;
+
+    if (action === 'delete') onDelete(articleId);
+    if (action === 'edit') onEdit(articleId);
+  };
+
   return (
-    <Table className={className} cellSpacing="0">
+    <Table className={className} cellSpacing="0" onClick={handleClick}>
       <THead>
         <TRow>
           {columnTitles.map((title, idx) => (
@@ -28,15 +51,19 @@ const ArticleList: FunctionComponent<ArticleListProps> = ({
       <TBody>
         {articles.map(
           ({ author, title, content, createdAt: date, id }, idx) => (
-            <StyledTRow key={idx}>
+            <StyledTRow key={idx} id={id}>
               <td>{author}</td>
               <td>{title}</td>
               <td>{content}</td>
               <td>{formatDateAsDDYYMM(date)}</td>
               <td>
                 <GridWrapper gap="1rem" templateColumns="repeat(2, auto)">
-                  <ActionButton action="edit">Edit</ActionButton>
-                  <ActionButton action="delete">Delete</ActionButton>
+                  <ActionButton className="action-button" action="edit">
+                    Edit
+                  </ActionButton>
+                  <ActionButton className="action-button" action="delete">
+                    Delete
+                  </ActionButton>
                 </GridWrapper>
               </td>
             </StyledTRow>
