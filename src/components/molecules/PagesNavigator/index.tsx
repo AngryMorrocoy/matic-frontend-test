6d7f9e29ca5fcbe2b19904d9d6b1f2593772d421
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-09-27 05:55:45 -0400

    + Arrows to PagesNavigator component

--- src/components/molecules/PagesNavigator/index.tsx
+++ src/components/molecules/PagesNavigator/index.tsx
@@ -1,22 +1,29 @@
 import NavButton from './PagesNavigator.styles';
 import { FlexWrapper } from '@atoms/Wrappers';
 import { FunctionComponent, useState, MouseEvent } from 'react';
+import { ReactComponent as LeftArrowIcon } from '@assets/icon-left-arrow.svg';
+import { ReactComponent as RightArrowIcon } from '@assets/icon-right-arrow.svg';
+import styled from 'styled-components';
 
 type ArticleListNavigatorProps = {
   pagesCount: number;
   currentPage: number;
   onPageChange: (newPage: number) => void;
+  className?: string;
 };
 
 const PagesNavigator: FunctionComponent<ArticleListNavigatorProps> = ({
   pagesCount,
   currentPage,
   onPageChange,
+  className
 }): JSX.Element => {
   const [showAllNavButtons, setShowAllNavButtons] = useState(false);
 
+  // An array with its index as values [0, 1, 2, 3, ..., n]
   let pages: number[] = [...Array(pagesCount).keys()];
   if (pagesCount > 7) {
+    // Insert -1 at position 4 if there are more than 7 pages
     pages = pages.slice(0, 3).concat([-1, ...pages.slice(3)]);
   }
 
@@ -34,27 +41,35 @@ const PagesNavigator: FunctionComponent<ArticleListNavigatorProps> = ({
   const getPropsForNavButton = (
     page: number
   ): { key: number; className: string; children: string } => {
-    let className: string = '';
+    let classes: string = '';
     if (page === 0) {
-      className = `navigator-toggler ${
+      classes = `navigator-toggler ${
         currentPage > 3 && currentPage <= pagesCount - 3 ? 'current' : ''
       }`;
-    } else if (page === currentPage) className = 'current';
+    } else if (page === currentPage) classes = 'current';
 
     return {
       key: page,
-      className,
+      className: classes,
       children: page === 0 ? '...' : `${page}`,
     };
   };
 
   return (
     <FlexWrapper
+      className={className}
       justify="space-between"
-      style={{ width: '100%' }}
       padding="1rem"
     >
-      <NavButton onClick={() => onPageChange(currentPage - 1)}>Prev</NavButton>
+      <FlexWrapper
+        as={NavButton}
+        onClick={() => onPageChange(currentPage - 1)}
+        align="center"
+        gap="0.8rem"
+      >
+        <LeftArrowIcon />
+        Prev
+      </FlexWrapper>
       <FlexWrapper
         className={`article-navigator__container ${
           showAllNavButtons ? 'show-all' : ''
@@ -68,9 +83,19 @@ const PagesNavigator: FunctionComponent<ArticleListNavigatorProps> = ({
           <NavButton {...getPropsForNavButton(value + 1)} />
         ))}
       </FlexWrapper>
-      <NavButton onClick={() => onPageChange(currentPage + 1)}>Next</NavButton>
+      <FlexWrapper
+        as={NavButton}
+        gap="0.8rem"
+        onClick={() => onPageChange(currentPage + 1)}
+        align="center"
+      >
+        Next
+        <RightArrowIcon />
+      </FlexWrapper>
     </FlexWrapper>
   );
 };
 
-export default PagesNavigator;
+export default styled(PagesNavigator)`
+  width: 100%;
+`;
