35e48d6c5e0a815e88c5b9a30a9c8ae3ad841d63
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-09-26 23:16:56 -0400

    + Styled table components

---
+++ src/components/atoms/Table/index.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  color: var(--table-default-fg);
+  text-align: left;
+  font-size: 0.8rem;
+  border-radius: 0.5rem;
+
+  box-shadow: var(--table-box-shadow);
+
+  & th,
+  & td {
+    padding: 1rem 1.5rem;
+  }
+
+  & tr:first-child th:first-child {
+    border-top-left-radius: 0.5rem;
+  }
+
+  & tr:first-child th:last-child {
+    border-top-right-radius: 0.5rem;
+  }
+
+  & tr:last-child td:first-child {
+    border-bottom-left-radius: 0.5rem;
+  }
+
+  & tr:last-child td:last-child {
+    border-bottom-right-radius: 0.5rem;
+  }
+`;
+
+export const TRow = styled.tr`
+  td,
+  th {
+    font-weight: 400;
+  }
+`;
+
+export const THead = styled.thead`
+  th,
+  td {
+    background-color: var(--table-heading-bg);
+    font-weight: 500;
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
+    text-transform: uppercase;
+    border-bottom: 1px solid var(--table-border-color);
+  }
+`;
+
+export const TBody = styled.tbody`
+  tr:nth-child(odd) th,
+  tr:nth-child(odd) td {
+    background-color: var(--table-odd-row-bg);
+  }
+
+  tr:nth-child(even) th,
+  tr:nth-child(even) td {
+    background-color: var(--table-even-row-bg);
+  }
+`;
